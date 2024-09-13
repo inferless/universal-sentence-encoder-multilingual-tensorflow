15e3dd193b9e0642e8c336aaebc385581f0ad5ff
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-13 23:28:43 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,8 +1,20 @@
 INPUT_SCHEMA = {
-    "prompt": {
+    "questions": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["What is an AI?"]
+        'example': ["What is your age?"]
+    },
+    "responses": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [2],
+        'example': ["I am 20 years old.","good morning"]
+    },
+    "response_contexts": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [2],
+        'example': ["I will be 21 next year.", "great day."]
     }
 }
